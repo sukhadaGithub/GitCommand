0a99096c26a2268aa4e2fbe91612f719f65833bd
unknown <sukhada.study7@gmail.com>
2024-07-19 17:26:13 +0530

    Scripted by tester1

---
+++ BDDFramePOM/src/test/java/Tester1/Demo1.java
@@ -0,0 +1,12 @@
+package Tester1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Scripted by Tester1");
+		// TODO Auto-generated method stub
+
+	}
+
+}
